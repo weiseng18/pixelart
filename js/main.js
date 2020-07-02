6444f0d517aa8b7067a64f22f5ffdc2f9fe80983
weiseng18 <tws.hci@gmail.com>
2020-07-02 13:48:03 +0800

    added color picker display but only the slider display has been implemented

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// ------
+// drawing area
+// ------
+
 function drawArea(height, width) {
 	this.height = height;
 	this.width = width;
@@ -27,10 +31,44 @@ drawArea.prototype.generateHTML = function() {
 	get("mainArea").appendChild(table);
 }
 
+// ------
+// color picker
+// ------
+
+function CanvasWrapper(id) {
+	this.HTML = get(id);
+	this.ctx = this.HTML.getContext("2d");
+	this.width = this.HTML.width;
+	this.height = this.HTML.height;
+}
+
+// creates a gradient of colors over the whole canvas
+// colors is the list of colors (must include the end color)
+// which is the direction
+CanvasWrapper.prototype.LinearGradient = function() {
+	var gradient = this.ctx.createLinearGradient(0, 0, 0, this.height);
+	gradient.addColorStop(0, "rgba(255, 0, 0, 1)");
+	gradient.addColorStop(0.166, "rgba(255, 255, 0, 1)");
+	gradient.addColorStop(0.333, "rgba(0, 255, 0, 1)");
+	gradient.addColorStop(0.5, "rgba(0, 255, 255, 1)");
+	gradient.addColorStop(0.666, "rgba(0, 0, 255, 1)");
+	gradient.addColorStop(0.833, "rgba(255, 0, 255, 1)");
+	gradient.addColorStop(1, "rgba(255, 0, 0, 1)");
+	this.ctx.fillStyle = gradient;
+	this.ctx.fillRect(0, 0, this.width, this.height);
+}
+// ------
+// main
+// ------
 
 var area;
+
+var slider;
 
 window.onload = function() {
 	area = new drawArea(20, 20);
 	area.generateHTML();
+
+	slider = new CanvasWrapper("color_slider");
+	slider.LinearGradient();
 };
