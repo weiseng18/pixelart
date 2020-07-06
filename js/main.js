7d9f853b6753bcc0e80a9207f886d3ea9d7f2a14
weiseng18 <tws.hci@gmail.com>
2020-07-06 19:50:27 +0800

    created skeleton for tool toggling 1. made table rows and columns equally distributed 2. created Tool object to store individual tools 3. created AddTool method to add new tools 4. added eyedropper tool icon, but the functionality has not been completely implemented

--- js/main.js
+++ js/main.js
@@ -483,11 +483,12 @@ ColorHistory.prototype.addColor = function(e, source) {
 // ------
 
 function ToolWrapper(rows, columns) {
-	// tentatively hardcoded size
 	var element = get("tool_wrapper");
 	var style = getComputedStyle(element);
-	this.height = style.height;
-	this.width = style.width;
+
+	// these two variables are numbers but the unit is px
+	this.height = parseInt(style.height.substring(0, style.height.length-2));
+	this.width = parseInt(style.width.substring(0, style.width.length-2));
 
 	this.rows = rows;
 	this.columns = columns;
@@ -496,6 +497,12 @@ function ToolWrapper(rows, columns) {
 
 	// add tools here
 	this.items = [];
+	this.total = 0;
+}
+
+ToolWrapper.prototype.addTool = function(Tool) {
+	this.items.push(Tool);
+	this.total++;
 }
 
 ToolWrapper.prototype.generateHTML = function() {
@@ -503,20 +510,52 @@ ToolWrapper.prototype.generateHTML = function() {
 	var table = document.createElement("table");
 	table.id = this.id;
 
-	table.style.height = this.height;
-	table.style.width = this.width;
+	table.style.height = this.height + "px";
+	table.style.width = this.width + "px";
 
 	table.style.tableLayout = "fixed";
 
+	console.log(this.height);
+
+	table.style.borderSpacing = 0;
+
 	for (var i=0; i<this.rows; i++) {
 		var row = table.insertRow();
+		row.style.height = (this.height / this.rows) + "px";
+
 		for (var j=0; j<this.columns; j++) {
 			var cell = row.insertCell();
-			cell.style.cursor = "pointer";
+
+			cell.style.verticalAlign = "middle";
+			cell.style.textAlign = "center";
+			var idx = i*this.columns + j;
+			if (idx < this.total) {
+				cell.style.cursor = "pointer";
+				cell.addEventListener("click", this.items[idx].click);
+
+				var img = new Image();
+				img.src = this.items[idx].iconSrc;
+
+				cell.appendChild(img);
+			}
 		}
 	}
 
 	get("tool_wrapper").appendChild(table);
+}
+
+function Tool(name, iconSrc) {
+	this.name = name;
+	this.iconSrc = "img/" + iconSrc;
+	this.click = function(e) {
+		var cell = e.target;
+		var column = cell.cellIndex;
+		var row = cell.parentElement.rowIndex;
+		var idx = row * tools.columns + column;
+
+		// set area.tool to idx unless it is already idx, which means to toggle it off
+		area.tool = (area.tool == idx ? 0 : idx);
+	}
 }
 
 // ------
@@ -526,6 +565,8 @@ ToolWrapper.prototype.generateHTML = function() {
 var area;
 
 var slider, body;
+
+var tools;
 
 var cHistory;
 
@@ -537,6 +578,10 @@ window.onload = function() {
 	get("color").style.backgroundColor = "rgb(0, 0, 0)";
 
 	tools = new ToolWrapper(4, 6);
+
+	var eyedropper = new Tool("eyedropper", "eyedropper.png");
+	tools.addTool(eyedropper);
+
 	tools.generateHTML();
 
 	slider = new CanvasWrapper("color_slider");
