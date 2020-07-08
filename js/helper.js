967803653cd8ae547b8c3266d1d23a02b4e792e2
weiseng18 <tws.hci@gmail.com>
2020-07-08 19:26:42 +0800

    add function to strip the 'px' off '400px' and convert to integer

--- js/helper.js
+++ js/helper.js
@@ -9,6 +9,11 @@ function init2D(height, width, val) {
 	return arr;
 }
 
+// convert 400px to 400 (integer)
+function removePX(string) {
+	return parseInt(string.substring(0, string.length-2));
+}
+
 // returns the HTML element of the xth tr of the yth td of a table.
 // expects id to be the id of a table.
 function getCell(id, x, y) {
