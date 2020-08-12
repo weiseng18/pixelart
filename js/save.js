25b767dc26401b29a04fc58e49cb9ab00b0fdbbf
weiseng18 <tws.hci@gmail.com>
2020-08-12 22:16:30 +0800

    [feature prep] added functions to save/load .frames

--- js/save.js
+++ js/save.js
@@ -34,6 +34,10 @@ function savePNG() {
 
 }
 
+// ------
+// .pix
+// ------
+
 // save raw data
 // current format is hex values separated by commas for pixels on the same row
 // and semicolons for row break
@@ -116,5 +120,128 @@ function handleRaw(data) {
 
 		area.updateGrid();
 		actionreplay.addState();
+	}
+}
+
+// ------
+// .frames
+// ------
+
+// number of frames : height : width : frame1 : frame2 : frame3 ...
+
+function saveFrames(which) {
+	var data = "";
+
+	data += frameWrapper.frames.length;
+	data += ":";
+	data += area.height;
+	data += ":";
+	data += area.width;
+	data += ":";
+
+	for (var f=0; f<frameWrapper.frames.length; f++) {
+		var grid = frameWrapper.frames[f].grid;
+
+		if (f) data += ":";
+
+		for (var i=0; i<area.height; i++) {
+			if (i) data += ";";
+			for (var j=0; j<area.width; j++) {
+				if (j) data += ".";
+				data += grid[i][j];
+			}
+		}
+	}
+
+	if (which == "localStorage") {
+		window.localStorage.setItem('frames', data);
+	}
+	else if (which == "download") {
+		// generate raw hex values of each pixel
+		var blob = new Blob([data], {type: 'text/plain'});
+		var href = URL.createObjectURL(blob);
+		var filename = 'raw.frames';
+		initiateDownload(href, filename);
+
+		// allow Blob to be deleted
+		URL.revokeObjectURL(href);
+	}
+}
+
+function loadFrames(which) {
+	if (which == "localStorage") {
+		var data = window.localStorage.getItem("frames");
+		handleFrames(data);
+	}
+	else if (which == "upload") {
+		var input = document.createElement("input");
+		input.style.display = "none";
+		input.type = "file";
+
+		input.addEventListener("change", function(e) {
+			// file reference
+			var file = e.target.files[0];
+
+			// set up file reader
+			var reader = new FileReader();
+			reader.readAsText(file, "UTF-8");
+
+			reader.addEventListener("load", function(e) {
+				var content = e.target.result;
+				handleFrames(content);
+			});
+
+		});
+
+		input.click();
+	}
+}
+
+function handleFrames(data) {
+	if (data != null) {
+		data = data.split(":");
+		var numFrames = parseInt(data[0]);
+		var height = parseInt(data[1]);
+		var width = parseInt(data[2]);
+
+		// just in case of size change
+		reload(height, width);
+
+		frameWrapper.frames = [];
+		// +3 due to the other variables in the file
+		for (var d=3; d<numFrames+3; d++) {
+			// reset area.grid
+			for (var i=0; i<height; i++)
+				for (var j=0; j<width; j++)
+					area.grid[i][j] = null;
+
+
+			frameWrapper.addNewFrame();
+			frameWrapper.loadFrame(d-3);
+
+			grid = data[d].split(";");
+			for (var i=0; i<height; i++)
+				grid[i] = grid[i].split(".");
+
+			for (var i=0; i<height; i++)
+				for (var j=0; j<width; j++) {
+					// localStorage stringifies stuff
+					if (grid[i][j] == "null")
+						area.grid[i][j] = null;
+					else
+						area.grid[i][j] = grid[i][j];
+				}
+
+			actionreplay.addState();
+		}
+
+		// delete innerHTML
+		get(frameWrapper.id).innerHTML = "";
+
+		// rewrite innerHTML
+		frameWrapper.initHTML();
+
+		this.whichFrame = 0;
+		frameWrapper.loadFrame(0, "handleFrames");
 	}
 }
